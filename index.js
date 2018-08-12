03a859ea1a5de1ecae31b16645aa064bba17935e
Neha Chopade <nehachopade22@gmail.com>
2018-08-12 14:06:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 /* Enter the code to remove the main node element under this comment */
+let main=document.querySelector('main')
+main.remove();
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+let newHeader = document.createElement('h1');
+newHeader.id="victory";
+newHeader.innerText="Neha-Chopade is the champion";
+document.body.appendChild(newHeader);
+
