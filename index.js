e2d8cb568c31ca278223b61ccd7c0b41ed9771d1
Neha Chopade <nehachopade22@gmail.com>
2018-08-12 14:11:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ let main=document.querySelector('main')
 main.remove();
 
 /* Create your new element here and assign it to newHeader */
+<<<<<<< HEAD
 let newHeader = document.createElement('h1');
 newHeader.id="victory";
 newHeader.innerText="Neha-Chopade is the champion";
 document.body.appendChild(newHeader);
+=======
+const newHeader = document.createElement('h1');
+newHeader.id="victory";
+newHeader.innerText="Neha Chopade is the champion";
+>>>>>>> e415275af8dcf9a0d32f5215e6a3e7aef622fc25
 
